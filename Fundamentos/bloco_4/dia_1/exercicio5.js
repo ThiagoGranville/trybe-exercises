1459c4acaee8bf99424807aa263ed8dd4a7033c1
Thiago Granville <thiagogranville@gmail.com>
2020-11-25 18:46:19 -0300

    Exercicio 5: Angulos internos de um triângulo

---
+++ Fundamentos/bloco_4/dia_1/exercicio5.js
@@ -0,0 +1,11 @@
+let angle1 = 40;
+let angle2 = 30;
+let angle3 = 110;
+
+if(angle1 + angle2 + angle3 === 180){
+  console.log(true);
+}
+else{
+  console.log(false);
+  console.log("Erro: Não é um triangulo.")
+}
