278c89f713481ce0c03f7826c33fbe0b272c4b5e
Thiago Granville <thiagogranville@gmail.com>
2020-11-25 19:30:32 -0300

    Exercicio 7: Converte notas em conceito

---
+++ Fundamentos/bloco_4/dia_1/exercicio7.js
@@ -0,0 +1,33 @@
+let nota = 80;
+
+if (nota < 0 || nota > 100){
+  console.log("ERRO: Nota inválida!");
+}
+
+else{
+  switch (true) {
+    case nota >= 90:
+      console.log('Nota: A');
+      break;
+
+    case nota >= 80:
+      console.log('Nota: B');
+      break;
+
+    case nota >= 70:
+      console.log('Nota: C');
+      break;
+
+    case nota >= 60:
+      console.log('Nota: D');
+      break;
+
+    case nota >= 50:
+      console.log('Nota: E');
+      break;
+
+    case nota < 50:
+      console.log('Nota: F');
+      break;
+  }
+}
