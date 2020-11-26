987f1d52597470e8f1d770cca2597bb9af91f7d4
Thiago Granville <thiagogranville@gmail.com>
2020-11-26 11:15:25 -0300

    Corrige exercicio 7, tirando codicional if e else

--- Fundamentos/bloco_4/dia_1/exercicio7.js
+++ Fundamentos/bloco_4/dia_1/exercicio7.js
@@ -1,33 +1,32 @@
-let nota = 80;
+let nota = 110;
 
-if (nota < 0 || nota > 100){
-  console.log("ERRO: Nota inválida!");
+switch (true) {
+  case nota < 0 || nota > 100:
+    console.log('ERRO: Nota inválida!');
+    break;
+
+  case nota >= 90:
+    console.log('Nota: A');
+    break;
+
+  case nota >= 80:
+    console.log('Nota: B');
+    break;
+
+  case nota >= 70:
+    console.log('Nota: C');
+    break;
+
+  case nota >= 60:
+    console.log('Nota: D');
+    break;
+
+  case nota >= 50:
+    console.log('Nota: E');
+    break;
+
+  case nota < 50:
+    console.log('Nota: F');
+    break;
 }
 
-else{
-  switch (true) {
-    case nota >= 90:
-      console.log('Nota: A');
-      break;
-
-    case nota >= 80:
-      console.log('Nota: B');
-      break;
-
-    case nota >= 70:
-      console.log('Nota: C');
-      break;
-
-    case nota >= 60:
-      console.log('Nota: D');
-      break;
-
-    case nota >= 50:
-      console.log('Nota: E');
-      break;
-
-    case nota < 50:
-      console.log('Nota: F');
-      break;
-  }
-}
