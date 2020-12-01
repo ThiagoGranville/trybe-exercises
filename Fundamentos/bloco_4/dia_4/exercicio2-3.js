a5f5713fe13227c7c9de74bf929b4b0518fe0a94
Thiago Granville <thiagogranville@gmail.com>
2020-12-01 11:20:53 -0300

    Exercicio 2.3: Cria função que descobre indice de menor valor em um array

---
+++ Fundamentos/bloco_4/dia_4/exercicio2-3.js
@@ -0,0 +1,15 @@
+let arrayTeste = [2, 4, 6, 7, 10, 0, -3];
+
+function indiceDeMenorValor(array) {
+  let menorValor = array[0];
+  let indexMenor = 0;
+  for (let index in array) {
+    if (array[index] < menorValor) {
+      menorValor = array[index];
+      indexMenor = index;
+    }
+  }
+  return indexMenor;
+}
+
+console.log(indiceDeMenorValor(arrayTeste));
