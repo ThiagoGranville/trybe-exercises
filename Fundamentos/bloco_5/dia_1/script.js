63569295f61e049dd67e0a8affdd9606436c3e1d
Thiago Granville <thiagogranville@gmail.com>
2020-12-02 21:00:27 -0300

    Exercicio 1: Cria função que muda texto de uma das tags de paragrafo

---
+++ Fundamentos/bloco_5/dia_1/script.js
@@ -0,0 +1,19 @@
+/*
+        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        - document.getElementById()
+        - document.getElementsByClassName()
+        - document.getElementsByTagName()
+*/
+
+//  Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+function changeText() {
+       let paragraph = document.getElementsByTagName('p')[1];
+       paragraph.innerText = 'Me vejo trabalhando com o que gosto e fazendo as pessoas a minha volta felizes';
+}
+changeText();
+//  Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+//  Crie uma função que mude a cor do quadrado vermelho para branco.
+//  Crie uma função que corrija o texto da tag <h1>.
+//  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+//  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+       
