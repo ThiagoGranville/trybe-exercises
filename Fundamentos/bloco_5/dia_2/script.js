52c0dffb59e73c2d8831c0c5c9a521758006f027
Thiago Granville <thiagogranville@gmail.com>
2020-12-04 12:04:53 -0300

    Exercicio 1.3: Cria elemento div com classe center-content como filho de main-content

--- Fundamentos/bloco_5/dia_2/script.js
+++ Fundamentos/bloco_5/dia_2/script.js
@@ -5,3 +5,7 @@ document.body.appendChild(h1Title);
 mainContent = document.createElement('div');
 mainContent.className = 'main-content';
 document.body.appendChild(mainContent);
+
+centerContent = document.createElement('div');
+centerContent.className = 'center-content';
+mainContent.appendChild(centerContent);
