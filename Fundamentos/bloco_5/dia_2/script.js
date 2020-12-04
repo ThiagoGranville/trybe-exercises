edb1d3fb772f52ca3b1b2d5959019d3d8ed9500b
Thiago Granville <thiagogranville@gmail.com>
2020-12-04 12:02:10 -0300

    Exercicio 1.2: Cria elemento div com classe main-content como filho do body

--- Fundamentos/bloco_5/dia_2/script.js
+++ Fundamentos/bloco_5/dia_2/script.js
@@ -1,3 +1,7 @@
 h1Title = document.createElement('h1');
 h1Title.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(h1Title);
+
+mainContent = document.createElement('div');
+mainContent.className = 'main-content';
+document.body.appendChild(mainContent);
