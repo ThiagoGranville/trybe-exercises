d31ef706f3ab765bcffbb6dee73aa12abbfc2bc0
Thiago Granville <thiagogranville@gmail.com>
2021-03-25 21:02:14 -0300

    Muda o local onde o BrowserRouter do exercicio 16.1 é importado

--- Front-End/Bloco_16/exercise16-1/src/App.js
+++ Front-End/Bloco_16/exercise16-1/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Cadastro from './pages/Cadastro';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,14 +8,12 @@ import ClientesCadastrados from './pages/ClientesCadastrados';
 class App extends Component {
   render() {
     return (
-      <Router>
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/login" component={ Login } />
           <Route path="/cadastro" component={ Cadastro } />
           <Route path='/users' component={ ClientesCadastrados } />
         </Switch>
-      </Router>
     );
   }
 }
